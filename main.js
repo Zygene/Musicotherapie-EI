daa456268e7f4c81cd4e2dd6d27530599e638849
Robin <wilmesrobin2001@hotmail.fr>
2026-02-23 20:09:03 +0100

    Sauvegarde

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const slide = document.getElementById("slide");
     const fleche = document.getElementById("fleche-retour");
     const contact = document.querySelector(".contact");
@@ -7,8 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (!slide || !bodyAccueil) return;
 
+    // Montrer le contenu principal quand le DOM est prêt
+    bodyAccueil.style.visibility = "visible";
+
     slide.style.transition = "transform 0.8s ease-in-out";
 
+    // Vérifie si la slide est déjà masquée
     if (slide.style.transform === "translateY(-100vh)") {
         bodyAccueil.style.overflowY = "auto";
     } else {
